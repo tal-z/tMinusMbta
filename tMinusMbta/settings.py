bd05649c88e3267a50986bc4b7daf2aa3b9af700
Tal Zaken <talzaken@gmail.com>
2021-11-23 18:31:15 -0500

    .

--- tMinusMbta/settings.py
+++ tMinusMbta/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x$!20&#(8(du5d7k*(n89=w)hv1=3s@tizlk3hkmuzx5#k$r*o' #os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
