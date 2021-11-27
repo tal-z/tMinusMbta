62ced86993a574fa98a84d6c7298180d607de7be
Tal Zaken <67941052+tal-z@users.noreply.github.com>
2021-11-27 11:57:50 -0500

    Update settings.py
    postres

--- tMinusMbta/settings.py
+++ tMinusMbta/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'tMinusMbta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': ‘<db_name>’,
+        'USER': '<db_username>',
+        'PASSWORD': '<password>',
+        'HOST': '<db_host>',
+        'PORT': '<db_port>',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
