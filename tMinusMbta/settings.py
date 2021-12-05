07a491ddb1a3e0a2867b3026b08bec9549367390
Tal Zaken <talzaken@gmail.com>
2021-12-04 21:17:04 -0500

    update static file settings and urls

--- tMinusMbta/settings.py
+++ tMinusMbta/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'timer',
     'users',
     'social_django',
+    'whitenoise',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -155,9 +156,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
