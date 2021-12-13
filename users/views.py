7036d7d9d3b802fc80b4d6390683e2d0a8750516
Tal Zaken <talzaken@gmail.com>
2021-12-13 13:26:16 -0500

    remove profile image

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from users.forms import CustomUserCreationForm, UserUpdateForm, ProfileUpdateForm, DashboardUpdateForm
+from users.forms import CustomUserCreationForm, UserUpdateForm, DashboardUpdateForm#, ProfileUpdateForm
 from users.models import UserTimer
 from timer.models import StoredTimer
 from timer.Timer import Timer, timers
@@ -137,21 +137,21 @@ def profile(request):
 def edit_profile(request):
     if request.method == "POST":
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        #p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
-        if u_form.is_valid() and p_form.is_valid():
+        if u_form.is_valid():# and p_form.is_valid():
             u_form.save()
-            p_form.save()
+            #p_form.save()
             messages.success(request, 'Your account has been updated!')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+        #p_form = ProfileUpdateForm(instance=request.user.profile)
     
 
     context = {
         'u_form': u_form, 
-        'p_form': p_form,
+        #'p_form': p_form,
     }
     return render(request, 'users/edit_profile.html', context=context)
 
